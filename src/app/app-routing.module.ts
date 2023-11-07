505b83dc42f652d743f436df79628a40c33e5847
Alberico Fagundes <alberico@fagundes.live>
2023-11-07 12:20:34 -0300

    correcao rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,30 @@ import { RouterModule, Routes } from '@angular/router';
 import { BasicLayoutComponent } from './shared/components/layouts/basic-layout/basic-layout.component';
 import { authenticationGuard } from './guards/authentication.guard';
 
+// const routes: Routes = [
+//   {
+//     path: '',
+//     redirectTo: 'home',
+//     pathMatch: 'full',
+//   },
+//   {
+//     path: 'home',
+//     component:BasicLayoutComponent,
+//     // canActivate: [authenticationGuard],
+//     // loadChildren: ()=> import('./shared/components/layouts/basic-layout')
+
+//   }
+// ];
+
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    component: BasicLayoutComponent,
   },
-  {
-    path: 'home',
-    component:BasicLayoutComponent,
-    // canActivate: [authenticationGuard],
-    // loadChildren: ()=> import('./shared/components/layouts/basic-layout')
-
-  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
